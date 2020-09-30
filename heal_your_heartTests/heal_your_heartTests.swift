87e74e9fdd5bbbe5920f74990b0e85dc0b4de0cd
shinohara-san <someiyoshino0809@gmail.com>
2020-09-30 11:21:58 +0900

    Initial Commit

---
+++ heal_your_heartTests/heal_your_heartTests.swift
@@ -0,0 +1,33 @@
+//
+//  heal_your_heartTests.swift
+//  heal_your_heartTests
+//
+//  Created by Yuki Shinohara on 2020/09/30.
+//
+
+import XCTest
+@testable import heal_your_heart
+
+class heal_your_heartTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
