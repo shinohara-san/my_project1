13146b02f20b7b1bd0e5c38d13009b596de1e7d4
shinohara-san <someiyoshino0809@gmail.com>
2020-10-06 11:28:31 +0900

    add login register function

--- heal_your_heart/Controllers/Action/LoginRegisterViewController.swift
+++ heal_your_heart/Controllers/Action/LoginRegisterViewController.swift
@@ -15,11 +15,22 @@ class LoginRegisterViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     
     @IBOutlet weak var registerStackView: UIStackView!
+    @IBOutlet weak var emailRegister: UITextField!
+    @IBOutlet weak var genderRegister: UITextField!
+    @IBOutlet weak var ageRegister: UITextField!
+    @IBOutlet weak var nickNameRegister: UITextField!
+    @IBOutlet weak var passRegister: UITextField!
+    @IBOutlet weak var passCheck: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loginStackView.isHidden = false
         registerStackView.isHidden = true
+        imageView.isUserInteractionEnabled = true
+        
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapChangeProfilePicture))
+        imageView.addGestureRecognizer(gesture)
     }
 
     @IBAction func didTapLogin(_ sender: Any) {
@@ -37,6 +48,38 @@ class LoginRegisterViewController: UIViewController {
             return
           }
             strongSelf.dismiss(animated: false, completion: nil)
+        }
+    }
+    @IBAction func didTapRegister(_ sender: Any) {
+        print("didTapRegister")
+        
+        guard let email = emailRegister.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let password = passRegister.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let checkedPassword = passCheck.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let age = ageRegister.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let gender = genderRegister.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              let nickname = nickNameRegister.text?.trimmingCharacters(in: .whitespacesAndNewlines),
+              !email.isEmpty, !password.isEmpty, !age.isEmpty, !gender.isEmpty, !nickname.isEmpty,
+              password == checkedPassword else {
+            alertUserLoginError(title: "エラー", message: "正しく情報を入力してください。")
+            return
+        }
+        
+        
+        
+        FirestoreManager.shared.storeUserInfo(nickname: nickname, gender: gender, age: age, email: email){ [weak self] success in
+            if success {
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    guard authResult != nil, error == nil else {
+                        self?.alertUserLoginError(title: "登録できませんでした。", message:  "正しく情報を入力してください。")
+                        return
+                    }
+                    self?.dismiss(animated: true, completion: nil)
+                }
+            } else {
+                self?.alertUserLoginError(title: "登録できませんでした。", message:  "正しく情報を入力してください。")
+                return
+            }
         }
     }
     
@@ -65,5 +108,53 @@ class LoginRegisterViewController: UIViewController {
         let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
         present(ac, animated: true)
+    }
+    
+    @objc private func didTapChangeProfilePicture(){
+        presentPhotoActionSheet()
+    }
+}
+
+extension LoginRegisterViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    private func presentPhotoActionSheet(){
+        let ac = UIAlertController(title: "Profile Picture", message: "How would you like to select a picture?", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        ac.addAction(UIAlertAction(title: "Take Photo", style: .default, handler: { [weak self] _ in
+            self?.presentCamera()
+        }))
+        ac.addAction(UIAlertAction(title: "Choose Photo", style: .default, handler: { [weak self] _ in
+            self?.presentPhotoPicker()
+        }))
+        
+        present(ac, animated: true)
+    }
+    
+    func presentCamera(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true //写真を四角で切り取れる
+        present(vc, animated: true)
+    }
+    
+    func presentPhotoPicker(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary //上のとの違い
+        vc.delegate = self
+        vc.allowsEditing = true //写真を四角で切り取れる
+        present(vc, animated: true)
+    }
+    
+    //以下二つがUIImagePickerControllerDelegateに関する関数
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        guard let selectedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {return}
+        imageView.image = selectedImage
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil) //閉じるやつ
     }
 }
--- heal_your_heart/Resources/Firebase/FirestoreManager.swift
+++ heal_your_heart/Resources/Firebase/FirestoreManager.swift
@@ -8,6 +8,27 @@
 import Foundation
 import FirebaseFirestore
 
-class FirestoreManager {
+final class FirestoreManager {
+    static let shared = FirestoreManager()
+    private init() {} //これ使うと同時にいろんなところで使えなくすることができる
+    private let db = Firestore.firestore()
+    
+    public func storeUserInfo(nickname: String, gender: String, age: String, email: String, completion: @escaping (Bool) -> Void){
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "nickname": nickname,
+            "gender": gender,
+            "age": age,
+            "email": email
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+                completion(false)
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+                completion(true)
+            }
+        }
+    }
     
 }
